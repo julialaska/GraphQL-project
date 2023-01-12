1e52788b3557ca5f6fb10f9e494b023657b6aafc
julialaska <julkalas101@gmail.com>
2023-01-12 18:32:35 +0100

    add urls

--- graphql_app/graphql_app/urls.py
+++ graphql_app/graphql_app/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 
 from graphene_django.views import GraphQLView
 from products.schema import schema
+
 urlpatterns = [
    path('admin/', admin.site.urls),
    path("graphql", GraphQLView.as_view(graphiql=True, schema=schema)),
